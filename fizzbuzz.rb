f9fd2516d5a6c34e2750678393e893123d45cb03
Jing Cao <jingcao.yale@gmail.com>
2019-01-09 20:32:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@ def fizzbuzz(num)
   if num % 3 == 0 && num % 5 != 0
     return "Fizz"
 
-  if num % 3 != 0 && num % 5 == 0
+  elsif num % 3 != 0 && num % 5 == 0
     return "Buzz"
 
-  if num % 3 != 0 && num % 5 == 0
+  elsif num % 3 != 0 && num % 5 == 0
     return "FizzBuzz"
   end
 
